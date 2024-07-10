8e465473311a5ed21db3c529e2f028d0be545ed9
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2024-07-10 20:36:29 -0300

    useState

--- app-curso/src/App.js
+++ app-curso/src/App.js
@@ -1,19 +1,58 @@
 import { useState } from 'react';
 
-import Nome from "./components/Nome";
-
 function App() {
-  const [nome, setNome] = useState('Gui')
 
-  function mudaNome() {
-    setNome('Gu')
+  const [titulo, setTitulo] = useState('')
+  const [duracao, setDuracao] = useState('')
+  const [capa, setCapa] = useState('')
+
+  const [filme, setFilme] = useState({})
+
+  function cadFilme(e) {
+    e.preventDefault()
+    alert("Cadastrado")
+
+    setFilme({
+      titulo: titulo,
+      duracao: duracao,
+      capa: capa
+    })
+
   }
 
   return (
     <div>
-      <h1>Opaaaaa</h1>
-      <Nome nome={nome} />
-      <button onClick={mudaNome}>Mudar nome</button>
+      <h1>Cadastro</h1>
+      <form onSubmit={cadFilme}>
+        <label>Titulo do filme</label><br />
+        <input
+          placeholder='ex: avatar'
+          value={titulo}
+          onChange={(e) => setTitulo(e.target.value)}
+        /><br /><br />
+
+        <label>Duração do filme</label><br />
+        <input
+          placeholder='ex: 155 minutos'
+          value={duracao}
+          onChange={(e) => setDuracao(e.target.value)}
+        /><br /><br />
+
+        <label>Capa do filme</label><br />
+        <input
+          placeholder='link da capa'
+          value={capa}
+          onChange={(e) => setCapa(e.target.value)}
+        /><br /><br />
+
+        <button type='submit'>Registrar</button>
+      </form>
+      <div>
+        <h1>Titulo {filme.titulo}</h1>
+        <p>Duração {filme.duracao} minutos</p>
+        <img src={filme.capa} />
+      </div>
+
     </div>
   );
 }
