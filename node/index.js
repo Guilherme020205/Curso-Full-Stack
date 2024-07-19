1276e12119f063865506f79307275218a71cad86
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2024-07-18 21:09:21 -0300

    .

--- node/index.js
+++ node/index.js
@@ -2,9 +2,23 @@ const express = require("express");
 
 const server = express();
 
+
+// Query params = ?nome=Nodejs
+// Route params = /curso/2
+// Request Body = { nome: 'NodeJS', tipo: 'Backend' }
+
+const cursos = ['ReactJS', 'JavaScript', 'NodeJS'] 
+
 // http://localhost:3000/curso
-server.get('/curso', (req, res) => {
-return res.json({curso: 'Hello World'})
-}) 
+server.get('/curso/:index', (req, res) => {
+
+    const index = req.params.index
+
+    return res.json(cursos[index])
+
+})
+
+
+
 
 server.listen(3000)
