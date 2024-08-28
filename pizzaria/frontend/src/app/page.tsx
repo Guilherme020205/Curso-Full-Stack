1c58a9e09988c98459031e36e78f213ce1f9bb4e
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2024-08-28 20:41:26 -0300

    OK

---
+++ pizzaria/frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1 className={styles.teste}>Teste </h1>
+      </main>
+    </div>
+  );
+}
