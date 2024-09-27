044c1018acdc228b13ad5b951b2eec6a4a2c3023
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2024-09-27 19:52:13 -0300

    Fazendo login

--- pizzaria/frontend/src/app/page.tsx
+++ pizzaria/frontend/src/app/page.tsx
@@ -2,8 +2,51 @@ import Link from "next/link";
 import styles from "./page.module.scss";
 import logoImg from '/public/logo.svg';
 import Image from 'next/image';
+import { api } from "@/services/api";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+
 
 export default function Home() {
+
+  async function handleLogin(formData: FormData) {
+      "use server"
+
+      const email = formData.get("email")
+      const password = formData.get("password")
+
+      if(email === "" || password === ""){
+        return;
+      }
+
+      try{
+
+        const response = await api.post("/session", {
+          email,
+          password
+        })
+        if(!response.data.token){
+          return;
+        }
+        console.log(response.data);
+
+        const expressTime = 60 * 60 * 24 * 30 * 1000;
+        cookies().set("session", response.data.token, {
+          maxAge: expressTime,
+          path: "/",
+          httpOnly: false,
+          secure: process.env.NODE_ENV === "production"
+        })
+
+      }catch(err){
+        console.log(err)
+        return;
+      }
+
+      redirect("/dashboard")
+
+  }
+
   return (
     <>
       <div className={styles.containerCenter}>
@@ -13,7 +56,7 @@ export default function Home() {
         />
 
         <section className={styles.login}>
-          <form>
+          <form action={handleLogin}> 
             <input
               className={styles.input}
               type="email"
