b77f8fb755f1803b57c6bc086157c38029a57f1c
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2024-12-09 21:03:15 -0300

    criado tela order

---
+++ pizzaria/mobile/src/pages/Order/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Order(){
+    return(
+        <View style={styles.container}>
+            <Text>Tela Order</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
