3eedab29f22ca2ad2d2ce68001f83e2eb2e42c02
Guilherme020205 <104910701+Guilherme020205@users.noreply.github.com>
2024-12-17 13:27:24 -0300

    ok

--- pizzaria/backend/src/services/order/SendOrderService.ts
+++ pizzaria/backend/src/services/order/SendOrderService.ts
@@ -1,25 +1,23 @@
 import prismaClient from "../../prisma";
 
-interface OrderRequest {
-    order_id: string;
+interface OrderRequest{
+  order_id: string;
 }
 
-class SendOrderService {
+class SendOrderService{
+  async execute({ order_id }: OrderRequest){
+    const order = await prismaClient.order.update({
+      where:{
+        id: order_id
+      },
+      data:{
+        draft: false
+      }
+    })
 
-    async execut({ order_id }: OrderRequest) {
-        const order = await prismaClient.order.update({
-            where: {
-                id: order_id
-            },
-            data: {
-                draft: false
-            }
-        })
+    return order;
 
-        return order
-
-    }
-
+  }
 }
 
 export { SendOrderService }
