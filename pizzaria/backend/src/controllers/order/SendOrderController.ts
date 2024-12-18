da83be75954141a987106baae58a8ebbc24fed36
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2024-12-17 21:24:00 -0300

    s

--- pizzaria/backend/src/controllers/order/SendOrderController.ts
+++ pizzaria/backend/src/controllers/order/SendOrderController.ts
@@ -1,19 +1,39 @@
-import { Response, Request } from "express";
-import { SendOrderService } from "../../services/order/SendOrderService";
+// import { Response, Request } from "express";
+// import { SendOrderService } from "../../services/order/SendOrderService";
 
 
-class SendOrderController {
-    async handle(req: Request, res: Response){
-        const {order_id} = req.body
+// class SendOrderController {
+//     async handle(req: Request, res: Response){
+//         const {order_id} = req.body
 
-        const sendOrder = new SendOrderService();
+//         const sendOrder = new SendOrderService();
 
-        const order = await sendOrder.execut({
-            order_id
-        })
+//         const order = await sendOrder.execut({
+//             order_id
+//         })
 
-        return res.json(order)
-    }
+//         return res.json(order)
+//     }
+// }
+
+// export { SendOrderController }
+
+import {Request, Response} from 'express'
+import { SendOrderService } from '../../services/order/SendOrderService'
+
+class SendOrderController{
+  async handle(req: Request, res: Response){
+    const { order_id } = req.body;
+
+    const sendOrder = new SendOrderService();
+
+    const order = await sendOrder.execute({
+      order_id
+    });
+
+    return res.json(order);
+
+  }
 }
 
 export { SendOrderController }
